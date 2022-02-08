6d4a54c19991a65e51dce7b3dc528fc7d8167c7e
Claudio Dornelles <dornelles.crtd@gmail.com>
2022-02-07 22:49:05 -0300

    Change Anime attribute ID from Integer to UUID

--- src/main/java/com/claudiodornelles/webflux/domain/Anime.java
+++ src/main/java/com/claudiodornelles/webflux/domain/Anime.java
@@ -6,10 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -17,12 +20,19 @@ import javax.validation.constraints.NotNull;
 @Builder
 @With
 @Table("anime")
-public class Anime {
+public class Anime implements Persistable<UUID> {
 
     @Id
-    private Integer id;
+    private UUID id;
+
     @NotNull
     @NotEmpty(message = "Anime name cannot be empty")
     private String name;
 
+    @Override
+    public boolean isNew() {
+        boolean result = Objects.isNull(id);
+        this.id = result ? UUID.randomUUID() : this.id;
+        return result;
+    }
 }
--- src/main/java/com/claudiodornelles/webflux/repository/AnimeRepository.java
+++ src/main/java/com/claudiodornelles/webflux/repository/AnimeRepository.java
@@ -2,7 +2,12 @@ package com.claudiodornelles.webflux.repository;
 
 import com.claudiodornelles.webflux.domain.Anime;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 public interface AnimeRepository extends ReactiveCrudRepository<Anime, Integer> {
+
+    Mono<Anime> findById(UUID id);
 
 }

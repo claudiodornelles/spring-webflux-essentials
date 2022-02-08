e7ac5b4b355d9c873c10e35d4dc8a7e2b774af6d
Claudio Dornelles <dornelles.crtd@gmail.com>
2022-02-07 22:53:11 -0300

    Add blockHound install

--- src/main/java/com/claudiodornelles/webflux/SpringWebfluxEssentialsApplication.java
+++ src/main/java/com/claudiodornelles/webflux/SpringWebfluxEssentialsApplication.java
@@ -2,9 +2,22 @@ package com.claudiodornelles.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringWebfluxEssentialsApplication {
+
+    @Bean
+    public WebProperties.Resources resources() {
+        return new WebProperties.Resources();
+    }
+
+    static {
+        BlockHound.install(builder ->
+                builder.allowBlockingCallsInside("java.util.UUID", "randomUUID"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringWebfluxEssentialsApplication.class, args);
